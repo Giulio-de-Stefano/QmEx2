7e2d0853942295b92d1d5b8ce2a13f3a747da89d
Giulio de Stefano <padremaronno89@gmail.com>
2017-06-18 17:12:25 +0100

    Added bids ByClassNMame

--- src/test/java/ebay/EbayElements.java
+++ src/test/java/ebay/EbayElements.java
@@ -5,9 +5,11 @@ import org.openqa.selenium.By;
 public class EbayElements {
     public static final By.ByClassName SEARCH_INPUT = new By.ByClassName("ui-autocomplete-input");
     public static final By.ById SEARCH_BTN = new By.ById("gh-btn");
+    public static final By.ByCssSelector AUCTION_FILTER_BTN = new By.ByCssSelector(".tgl_button.center_b");
     public static final By.ById RESULT_LIST = new By.ById("ListViewInner");
 
     public static final By.ByClassName RESULT_ITEM = new By.ByClassName("sresult");
     public static final By.ByClassName RESULT_ITEM_PRICE = new By.ByClassName("lvprice");
     public static final By.ByClassName RESULT_ITEM_TITLE = new By.ByClassName("vip");
+    public static final By.ByClassName RESULT_ITEM_BIDS = new By.ByClassName("lvformat");
 }
